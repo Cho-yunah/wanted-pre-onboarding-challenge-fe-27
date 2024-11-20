9c6519468631f87b46fa928f464da3f6bb62d944
yunah <favoryun12@gmail.com>
2024-11-20 17:26:39 +0900

    Feat. Auth layout 설정 - style 파일 설정 - 기본 auth layout 설정

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -3,11 +3,8 @@ import { Outlet } from "react-router-dom";
 
 const AuthLayout = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-blue-50">
-      <div className="w-full max-w-md bg-white shadow-lg rounded-lg p-8">
-        <header className="text-2xl font-bold text-center mb-6 text-red-800">
-          로그인 / 회원가입
-        </header>
+    <div className="card h-screen w-screen bg-[#FBFFF7] p-2">
+      <div className="w-full max-w-lg shadow-lg bg-white rounded-xl p-8">
         <main>
           <Outlet />
         </main>
