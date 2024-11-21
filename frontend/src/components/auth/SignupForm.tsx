91c7757d09dc79d1b0a251eab62fdfb17709a96f
yunah <favoryun12@gmail.com>
2024-11-21 16:33:05 +0900

    Feat. signup form validation 개발

--- frontend/src/components/auth/SignupForm.tsx
+++ frontend/src/components/auth/SignupForm.tsx
@@ -1,33 +1,101 @@
-import React from "react";
+import {
+  extractErrors,
+  getInputClassName,
+} from "../../config/utils/validation";
+import { useState } from "react";
+import { z } from "zod";
+import ErrorMessage from "./ErrorMessage";
+
+interface SignupFormErrors {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const schema = z.object({
+  name: z.string().min(2, "2글자 이상의 이름을 입력해주세요."),
+  email: z.string().email("유효한 이메일 주소를 입력해주세요."),
+  password: z.string().min(8, "비밀번호는 8자 이상이어야 합니다."),
+});
 
 const SignupForm = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e: any) => {
+    console.log(e.target.name, e.target.value);
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    const result = schema.safeParse(formData);
+    if (!result.success) {
+      const fieldErrors = extractErrors(result.error);
+      setErrors({
+        name: fieldErrors.name || "",
+        email: fieldErrors.email || "",
+        password: fieldErrors.password || "",
+      });
+      return;
+    }
+    setErrors({} as SignupFormErrors);
+  };
+
   return (
     <div>
       <div className="text-center p-6 align-center">
         <h1 className="text-2xl font-bold">Create Account!</h1>
         <p className="text-gray-500">Sign up to get Started!</p>
       </div>
-      <form className="flex flex-col space-y-6 mt-2 mb-1">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col space-y-2 mt-1 mb-1"
+      >
         <input
-          type="name"
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
           placeholder="Your Name"
-          className="border-none bg-[#f8f9fa] shadow-inner p-3 rounded-lg"
+          className={getInputClassName(!!errors.name)}
         />
+        <ErrorMessage message={errors.name} />
+
         <input
           type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
           placeholder="Your Email"
-          className="border-none bg-[#f8f9fa] shadow-inner p-3 rounded-lg"
+          className={getInputClassName(!!errors.email)}
         />
+        <ErrorMessage message={errors.email} />
+
         <input
           type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
           placeholder="Password"
-          className="border-none bg-[#f8f9fa] shadow-inner p-3 rounded-lg"
+          className={getInputClassName(!!errors.password)}
         />
+        <ErrorMessage message={errors.password} />
+
         <button
           type="submit"
-          className="bg-lime-500 p-3 rounded-lg text-white font-bold"
+          className="bg-emerald-400 p-3 rounded-lg text-white font-bold"
         >
-          Login
+          Sign up
         </button>
       </form>
     </div>
